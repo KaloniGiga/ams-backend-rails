a9c9a0414fa58a1c154149d3284124ff0564d274
kalaunidipak <kalaunidipak5@gmail.com>
2024-09-03 05:07:33 +0000

    fix: session disabled

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionsFix
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
@@ -47,7 +48,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:password, :email, :first_name, :last_name, :dob, :address, :gender, :role_type, :phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:password, :current_password, :email, :first_name, :last_name, :dob, :address, :gender, :role_type, :phone])
   end
 
   # The path used after sign up.
